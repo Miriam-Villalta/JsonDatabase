80f22de0f599819d54b52b97a174245c966dfc61
Miriam-Villalta <mvb_8@hotmail.com>
2023-04-11 10:11:50 +0200

    clean comments and use ServerCall class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,7 @@
-import org.h2.tools.Server;
-
-import java.io.BufferedReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 import java.util.List;
-import java.util.Scanner;
+import ServerCall.ServerCall;
 
 public class Main {
     public static <BufferReader> void main(String[] args) throws Exception {
@@ -19,32 +11,14 @@ public class Main {
 
         Path path = FileSystems.getDefault().getPath("C:/Users/Miriam/IdeaProjects/ingresos.json");
 
-
         List<Ingreso> ingresos = CsvToJson.fromCsv(pathFiles, fileIncome);
 
         CsvToJson.toJson(ingresos, pathFiles + fileJson);
         List<Ingreso> json = CsvToJson.readJson(path);
         CsvToJson.insert(json);
 
-        /*ServerCall server = new ServerCall();
-        server.serverCall();*/
-        Server server = Server.createWebServer().start();
-        Server.openBrowser(server.getURL());
+        ServerCall server = new ServerCall();
+        server.serverCall();
 
-        System.out.println("Pulsa enter para finalizar");
-        (new Scanner(System.in)).nextLine();
-        server.stop();
-
-        /* url = "jdbc:h2:~/test";
-        String user = "sa";
-        String password = "";
-
-        Connection connection = CsvToJson.getConnection();
-        Statement command = connection.createStatement();
-        String sql1 = "";
-
-        command.execute(""); //para insertar
-        command.close();
-        connection.close();*/
     }
 }
